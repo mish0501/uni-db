b0e5c9d833d1fd0f704dbc9aa7a05e4b097dc99a
Mihail Georgiev <mmisho0501@gmail.com>
2022-03-14 12:24:21 +0200

    Split year 2 and 3 to different branches

--- transactions_by_client_name.sql
+++
@@ -1,6 +0,0 @@
-SELECT transactions.*
-FROM transactions
-  INNER JOIN bank_accounts ON transactions.bank_account_id = bank_accounts.id
-  INNER JOIN clients ON clients.id = bank_accounts.client_id
-WHERE clients.name LIKE '%%'
-ORDER BY transactions.created_at;
