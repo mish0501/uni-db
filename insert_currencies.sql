11def482ba40b53590fdd0b3130aae624bc0a6b7
Mihail Georgiev <mmisho0501@gmail.com>
2022-03-14 12:31:02 +0200

    Clean up unnecessary files

--- insert_currencies.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO currency_types(`name`, `symbol`)
-VALUES ('Bulgarian lev', 'лв.'),
-  ('Euro', '€'),
-  ('United States dollar', '$'),
-  ('Japanese yen', '¥'),
-  ('Pound sterling', '£');
--- insert_transaction_types.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `transaction_types` (`name`)
-VALUES ('ATM'),
-  ('Charge'),
-  ('Check'),
-  ('Deposit'),
-  ('Online'),
-  ('POS'),
-  ('Transfer'),
-  ('Withdrawal');
