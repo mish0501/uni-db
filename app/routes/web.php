f6034bd61a27ca91224c4b312ceae2a6991a7967
Mihail Georgiev <mmisho0501@gmail.com>
2022-03-31 21:24:57 +0300

    Change to breeze vue with inertia

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::view('profile', 'profile')->name('profile');
-    Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
-});
-
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
